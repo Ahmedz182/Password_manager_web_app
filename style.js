a074e134902899cd40e6834e8500a818a26af82a
ahmedz182 <ahmedmughal3182@gmail.com>
2023-01-19 16:52:17 +0500

    show button

--- style.js
+++ style.js
@@ -1,15 +1,5 @@
-
-
 document.getElementById('button').addEventListener("click", addpass);
-document.getElementById('show').addEventListener("click", showpass);
-function showpass() {
-    if (tdpass.className == ('hidetext')) {
-        tdpass.classList.remove('hidetext');
-    } else {
-        tdpass.classList.add('hidetext');
-    }
 
-}
 function addpass(e) {
     var weblink = document.getElementById('link').value
     var email = document.getElementById('emails').value
@@ -28,40 +18,55 @@ function addpass(e) {
     tdemail.innerText = email;
 
     const tduser = document.createElement('td');
-    tduser.setAttribute("id", "td5");
+    tduser.setAttribute("id", "td3");
     tduser.innerText = user;
 
     const tdpass = document.createElement('td');
-    tdpass.setAttribute("id", "td3");
+    tdpass.setAttribute("id", "td4");
     tdpass.classList.add('hidetext');
     tdpass.innerText = pass;
-    const edit = document.createElement('i');
-    edit.classList.add("fa", "fa-eye-slash");
-    edit.setAttribute("id", "show");
 
-    const act = document.createElement('td');
-    act.setAttribute("id", "td4");
+    const show = document.createElement('button');
+    show.classList.add("btn", "fa", "fa-eye-slash");
+    show.setAttribute("id", "shows");
 
+    const trash = document.createElement('button');
+    trash.classList.add("btn", "fa", "fa-trash");
+    trash.setAttribute("id", "del");
 
-
-    const trash = document.createElement('i');
-    trash.classList.add("fa", "fa-trash");
 
     document.getElementById("tab").appendChild(trw);
     document.getElementById("tab").appendChild(tdweb);
     document.getElementById("tab").appendChild(tdemail);
     document.getElementById("tab").appendChild(tduser);
     document.getElementById("tab").appendChild(tdpass);
-    document.getElementById("tab").appendChild(edit);
+    document.getElementById("tab").appendChild(show);
     document.getElementById("tab").appendChild(trash);
 
 
     var weblink = document.getElementById('link').value = ''
     var email = document.getElementById('emails').value = ''
     var pass = document.getElementById('password').value = ''
-
     var user = document.getElementById('Username').value = ''
 
+    document.getElementById('shows').addEventListener("click", showpass);
+    document.getElementById('del').addEventListener("click", deleted);
+    function showpass() {
+
+        if (tdpass.classList.contains = 'hidetext') {
+            tdpass.classList.replace('hidetext', 'show');
+            show.classList.replace("fa-eye-slash", "fa-eye");
+        }
+
+        else if (tdpass.classList.contains = 'show') {
+            tdpass.classList.replace('show', 'hidetext');
+            show.classList.replace("fa-eye", "fa-eye-slash");
+        }
+
+    }
+    function deleted() {
+        trw.remove();
+    }
     e.preventDefault();
 
 
